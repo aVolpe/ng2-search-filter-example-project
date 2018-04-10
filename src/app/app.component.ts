0767e66f0d1af80b66099ee867b49bf08c73c7de
Arturo Volpe <arturovolpe@gmail.com>
2018-04-09 20:52:24 -0400

    Example with two filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
+    firstName : string;
+    lastName: string;
+
+    items: any[] = [
+      {name: 'archie', lastName: 'test1'},
+      {name: 'archie', lastName: 'test4'},
+      {name: 'jake', lastName: 'test2'},
+      {name: 'richard', lastName: 'test3'}];
 }
